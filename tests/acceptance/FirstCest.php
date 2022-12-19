58b72ff77ee2cdab843306e11d053d61a84fce51
infernosquad <infzanoza@gmail.com>
2022-12-19 14:58:40 +0800

    Add codeception

---
+++ tests/acceptance/FirstCest.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Tests\Acceptance;
+
+use App\Tests\AcceptanceTester;
+
+class FirstCest
+{
+    public function _before(AcceptanceTester $I)
+    {
+    }
+
+    // tests
+    public function tryToTest(AcceptanceTester $I)
+    {
+        $I->amOnPage('/');
+        $I->see('Hello');
+    }
+}
