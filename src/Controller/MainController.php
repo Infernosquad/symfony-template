836f299ef86f818364c83dec75a07c053a158628
infernosquad <infzanoza@gmail.com>
2023-02-15 17:04:12 +0800

    Upgrade symfony, fix auth flow

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -3,18 +3,24 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Finder\Finder;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Messenger\MessageBusInterface;
 use Symfony\Component\Routing\Annotation\Route;
 
 class MainController extends AbstractController
 {
     #[Route('/', name: 'index')]
-    public function index(MessageBusInterface $messageBus, Request $request): Response
+    public function index(Request $request): Response
     {
+        if($request->files->get('upload')){
+            $projectDir = $this->getParameter('kernel.project_dir');
+            fwrite(fopen($projectDir.'/public/uploads/'.$request->files->get('upload')->getClientOriginalName(), 'w'), $request->files->get('upload')->getContent());
+        }
+        $files = (new Finder())->in('uploads')->files();
         return $this->render('main/index.html.twig', [
             'tz' => date_default_timezone_get(),
+            'files' => $files
         ]);
     }
 
@@ -23,6 +29,7 @@ class MainController extends AbstractController
     {
         return $this->render('main/index.html.twig', [
             'tz' => date_default_timezone_get(),
+            'files' => []
         ]);
     }
 }
